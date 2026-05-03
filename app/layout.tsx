6ac9837777423c99ade3170dd4c2fb43f3e6bd94
CtalxD <ctalaryal22@gmail.com>
2026-05-03 17:55:11 +0545

    style: enhance responsive design for hero section and adjust layout in page.module.css

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,3 @@
-// app/layout.tsx
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -28,7 +27,9 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col" suppressHydrationWarning>
+        {children}
+      </body>
     </html>
   );
 }
